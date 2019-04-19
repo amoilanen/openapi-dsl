87fe57ef8cacf98176052150d07d301ceb639c40
Anton Ivanov <anton.al.ivanov@gmail.com>
2019-04-19 14:49:09 +0300

    Add documentation/update license

--- src/openapi.ts
+++ src/openapi.ts
@@ -1,6 +1,20 @@
+/*
+ * Typescript-based DSL for defining OpenAPI specifications.
+ * Documentation comments and the API structure from https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md
+ */
+
 class Contact {
+  /**
+   * The identifying name of the contact person/organization.
+   */
   name: string
+  /**
+   * The URL pointing to the contact information. MUST be in the format of a URL.
+   */
   url?: string
+  /**
+   * The email address of the contact person/organization. MUST be in the format of an email address.
+   */
   email: string
   constructor({ name, url, email}: Contact) {
     this.name = name;
@@ -9,10 +23,19 @@ class Contact {
   }
 }
 
+/**
+ * Contact information for the exposed API.
+ */
 export const contact = (contact: Contact) => new Contact(contact);
 
 class License {
+  /**
+   * The license name used for the API.
+   */
   name: string
+  /**
+   * A URL to the license used for the API. MUST be in the format of a URL.
+   */
   url?: string
   constructor({ name, url }: License) {
     this.name = name;
@@ -20,14 +43,35 @@ class License {
   }
 }
 
+/**
+ * License information for the exposed API.
+ */
 export const license = (license: License) => new License(license);
 
 class Info {
+  /**
+   * The title of the application.
+   */
   title: string
-  license: License
+  /**
+   * The license information for the exposed API.
+   */
+  license?: License
+  /**
+   * The version of the OpenAPI document (which is distinct from the OpenAPI Specification version or the API implementation version).
+   */
   version: string
+  /**
+   * A short description of the application. CommonMark syntax MAY be used for rich text representation.
+   */
   description?: string
+  /**
+   * A URL to the Terms of Service for the API. MUST be in the format of a URL.
+   */
   termsOfService?: string
+  /**
+   * The contact information for the exposed API.
+   */
   contact?: Contact
   constructor({ title, description, termsOfService, contact, license, version }: Info) {
     this.title = title;
@@ -39,11 +83,25 @@ class Info {
   }
 }
 
+/**
+ * Provides metadata about the API. The metadata MAY be used by tooling as required.
+ */
 export const info = (info: Info) => new Info(info);
 
 class ServerVariable {
+  /**
+   * An enumeration of string values to be used if the substitution options are from a limited set.
+   */
   _enum?: string[]
+  /**
+   * The default value to use for substitution, which SHALL be sent if an alternate value is not supplied.
+   * Note this behavior is different than the Schema Object's treatment of default values,
+   * because in those cases parameter values are optional.
+   */
   _default: string
+  /**
+   * An optional description for the server variable. CommonMark syntax MAY be used for rich text representation.
+   */
   description?: string
   constructor({ _enum, _default, description }: ServerVariable) {
     this._enum = _enum;
@@ -52,11 +110,23 @@ class ServerVariable {
   }
 }
 
+/**
+ * An object representing a Server Variable for server URL template substitution.
+ */
 export const serverVariable = (serverVariable: ServerVariable) => new ServerVariable(serverVariable);
 
 class Server {
+  /**
+   * A URL to the target host. This URL supports Server Variables and MAY be relative, to indicate that the host location is relative to the location where the OpenAPI document is being served. Variable substitutions will be made when a variable is named in {brackets}.
+   */
   url: string
+  /**
+   * An optional string describing the host designated by the URL. CommonMark syntax MAY be used for rich text representation.
+   */
   description?: string
+  /**
+   * A map between a variable name and its value. The value is used for substitution in the server's URL template.
+   */
   variables?: { [name: string]: ServerVariable }
   constructor({ url, description, variables }: Server) {
     this.url = url;
@@ -65,6 +135,9 @@ class Server {
   }
 }
 
+/**
+ * An object representing a Server.
+ */
 export const server = (server: Server) => new Server(server);
 
 class Api {
@@ -74,7 +147,15 @@ class Api {
    * This is not related to the API info.version string.
    */
   openapi: string
+  /**
+   * Provides metadata about the API. The metadata MAY be used by tooling as required.
+   */
   info: Info
+  /**
+   * An array of Server Objects, which provide connectivity information to a target server. 
+   * If the servers property is not provided, or is an empty array,
+   * the default value would be a Server Object with a url value of /.
+   */
   servers?: Server[]
   constructor({ openapi, info, servers }: Api) {
     this.openapi = openapi;
