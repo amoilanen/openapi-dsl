a76f1d9ed63a4b6d992a39115ee9c30f892b520f
Anton Ivanov <anton.al.ivanov@gmail.com>
2019-04-22 22:23:44 +0300

    Add more definitions

--- src/openapi.ts
+++ src/openapi.ts
@@ -160,19 +160,191 @@ class ExternalDocs {
  */
 export const externalDocs = (externalDocs: ExternalDocs) => new ExternalDocs(externalDocs);
 
-//TODO:
 class Parameter {
+  /**
+   *  The name of the parameter. Parameter names are case sensitive.
+   * If in is "path", the name field MUST correspond to the associated path segment from the path field in the Paths Object.
+   * See Path Templating for further information. If in is "header" and the name field is "Accept", "Content-Type" or "Authorization",
+   * the parameter definition SHALL be ignored. For all other cases,
+   * the name corresponds to the parameter name used by the in property.
+   */
+  name: string
+  /**
+   * The location of the parameter. Possible values are "query", "header", "path" or "cookie".
+   */
+  _in: string
+  /**
+   * A brief description of the parameter. This could contain examples of use. CommonMark syntax MAY be used for rich text representation.
+   */
+  description?: string
+  /**
+   * Determines whether this parameter is mandatory. If the parameter location is "path", this property is REQUIRED and its value MUST be true.
+   * Otherwise, the property MAY be included and its default value is false.
+   */
+  required?: boolean
+  /**
+   * Specifies that a parameter is deprecated and SHOULD be transitioned out of usage. Default value is false.
+   */
+  deprecated?: boolean
+  /**
+   * Sets the ability to pass empty-valued parameters. This is valid only for query parameters and
+   * allows sending a parameter with an empty value. Default value is false.
+   * If style is used, and if behavior is n/a (cannot be serialized), the value of allowEmptyValue SHALL be ignored.
+   * Use of this property is NOT RECOMMENDED, as it is likely to be removed in a later revision.
+   */
+  allowEmptyValue: boolean
+  constructor({ name, _in, description, required, deprecated, allowEmptyValue }: Parameter) {
+    this.name = name;
+    this._in = _in;
+    this.description = description;
+    this.required = required;
+    this.deprecated = deprecated;
+    this.allowEmptyValue = allowEmptyValue;
+  }
+}
+
+/**
+ * Describes a single operation parameter.
+ * A unique parameter is defined by a combination of a name and location.
+ */
+export const parameter = (parameter: Parameter) => new Parameter(parameter);
+
+class Reference {
+  /**
+   * The reference string.
+   */
+  $ref: string
+  constructor({ $ref }: Reference) {
+    this.$ref = $ref;
+  }
+}
+
+/**
+ * A simple object to allow referencing other components in the specification, internally and externally.
+ * The Reference Object is defined by JSON Reference and follows the same structure, behavior and rules.
+ * For this specification, reference resolution is accomplished as defined by the JSON Reference specification and not by the JSON Schema specification.
+ */
+export const reference = (reference: Reference) => new Reference(reference);
+
+//TODO:
+class Schema {
+  title
+  multipleOf
+  maximum
+  exclusiveMaximum
+  minimum
+  exclusiveMinimum
+  maxLength
+  minLength
+  pattern
+  maxItems
+  minItems
+  uniqueItems
+  maxProperties
+  minProperties
+  required
+  enum
+  /**
+   * Value MUST be a string. Multiple types via an array are not supported.
+   */
+  type: string
+  /**
+   * Inline or referenced schema MUST be of a Schema Object and not a standard JSON Schema.
+   */
+  allOf?: Schema
+  /**
+   * Inline or referenced schema MUST be of a Schema Object and not a standard JSON Schema.
+   */
+  oneOf?: Schema
+  /**
+   * Inline or referenced schema MUST be of a Schema Object and not a standard JSON Schema.
+   */
+  anyOf?: Schema
+  /**
+   * Inline or referenced schema MUST be of a Schema Object and not a standard JSON Schema.
+   */
+  not?: Schema
+  /**
+   * Value MUST be an object and not an array. Inline or referenced schema MUST be of a Schema Object and not a standard JSON Schema. items MUST be present if the type is array.
+   */
+  items?: Schema[]
+  /**
+   * Property definitions MUST be a Schema Object and not a standard JSON Schema (inline or referenced).
+   */
+  properties?: Schema
+  /**
+   * 
+   */
+  additionalProperties: Schema
 }
 
 //TODO:
-class Reference {
+class Example {
 
 }
 
-//TODO
+//TODO:
+class Encoding {
+
+}
+
+class MediaType {
+  /**
+   * The schema defining the content of the request, response, or parameter.
+   */
+  schema: Schema | Reference
+  /**
+   * Example of the media type. The example object SHOULD be in the correct format as specified by the media type.
+   * The example field is mutually exclusive of the examples field. Furthermore, if referencing a schema which contains an example,
+   * the example value SHALL override the example provided by the schema.
+   */
+  example?: any
+  /**
+   * Examples of the media type. Each example object SHOULD match the media type and specified schema if present.
+   * The examples field is mutually exclusive of the example field. Furthermore, if referencing a schema which contains an example,
+   * the examples value SHALL override the example provided by the schema.
+   */
+  examples?: { [name : string]: (Example | Reference) }
+  /**
+   * A map between a property name and its encoding information. The key, being the property name,
+   * MUST exist in the schema as a property. The encoding object SHALL only apply to requestBody
+   * objects when the media type is multipart or application/x-www-form-urlencoded.
+   */
+  encoding: { [name: string]: Encoding }
+  constructor({ schema, example, examples, encoding }: MediaType) {
+    this.schema = schema;
+    this.example = example;
+    this.examples = examples;
+    this.encoding = encoding;
+  }
+}
+
+/**
+ * Each Media Type Object provides schema and examples for the media type identified by its key.
+ */
+export const mediaType = (mediaType: MediaType) => new MediaType(mediaType);
+
 class RequestBody {
-
+  /**
+   * A brief description of the request body. This could contain examples of use. CommonMark syntax MAY be used for rich text representation.
+   */
+  description?: string
+  content: { [mediaType: string]: MediaType }
+  /**
+   * Determines if the request body is required in the request. Defaults to false.
+   */
+  required?: boolean
+  constructor({ description, content, required }: RequestBody) {
+    this.description = description;
+    this.content = content;
+    this.required = required;
+  }
 }
+
+/**
+ * Describes a single request body.
+ */
+export const requestBody = (requestBody: RequestBody) => new RequestBody(requestBody);
 
 //TODO
 class Responses {
